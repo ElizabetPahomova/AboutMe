e6496eedb2c38a50bac2a4c0822276580bfd94a7
ElizabetPahomova <aroslave545@gmail.com>
2024-03-29 02:40:07 +0300

    version 1.0

---
+++ script.js
@@ -0,0 +1,33 @@
+// const burger = document.querySelector('.burger');
+// const nav = document.querySelector('.nav');
+// const footerLinks = document.querySelectorAll('.footer .links li');
+// const headerLinks = document.querySelectorAll('.header .links li');
+//
+// burger.addEventListener('click', () => {
+//     burger.classList.toggle('active');
+//     nav.classList.toggle('active');
+// });
+//
+// footerLinks.forEach((link, index) => {
+//     const headerLink = headerLinks[index];
+//     const clone = link.cloneNode(true);
+//     headerLink.appendChild(clone);
+// });
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+const footerLinks = document.querySelectorAll('.footer .links li');
+const headerLinks = document.querySelectorAll('.header .links li');
+
+if (window.matchMedia('(max-width: 768px)').matches) {
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    nav.classList.toggle('active');
+  });
+
+  footerLinks.forEach((link, index) => {
+    const headerLink = headerLinks[index];
+    const clone = link.cloneNode(true);
+    headerLink.appendChild(clone);
+  });
+}
