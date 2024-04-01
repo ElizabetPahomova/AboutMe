ee36918a0266366b3c469ef26b35e9f8b5542c83
ElizabetPahomova <aroslave545@gmail.com>
2024-04-02 01:14:47 +0300

    version 3.0 - fix burger menu and little bit bugs

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
-// const burger = document.querySelector('.burger');
-// const nav = document.querySelector('.nav');
-// const footerLinks = document.querySelectorAll('.footer .links li');
-// const headerLinks = document.querySelectorAll('.header .links li');
-//
-// burger.addEventListener('click', () => {
-//     burger.classList.toggle('active');
-//     nav.classList.toggle('active');
-// });
-//
-// footerLinks.forEach((link, index) => {
-//     const headerLink = headerLinks[index];
-//     const clone = link.cloneNode(true);
-//     headerLink.appendChild(clone);
-// });
-
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav');
+const navLinks = document.querySelector('.nav .links');
 const footerLinks = document.querySelectorAll('.footer .links li');
 const headerLinks = document.querySelectorAll('.header .links li');
-
+console.log(headerLinks)
 if (window.matchMedia('(max-width: 768px)').matches) {
-    burger.addEventListener('click', () => {
-        burger.classList.toggle('active');
-        nav.classList.toggle('active');
-    });
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    nav.classList.toggle('active');
+  });
+  footerLinks.forEach((link, index) => {
+    const clone = link.cloneNode(true);
+    navLinks.appendChild(clone);
+  });
+  const img = document.getElementById("myImg");
+  img.style.maxWidth = "40%";
 
-    const lastHeaderLink = headerLinks[headerLinks.length - 1]
-    footerLinks.forEach((link, index) => {
-        lastHeaderLink.appendChild(link);
-    });
 }
+
